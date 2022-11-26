4048f71e7dd5327b333c772ff9441ae185943baf
shivambadoni19992 <shivambadoni.39@gmail.com>
2022-11-26 15:53:17 +0530

    Assign

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.util.*;
+import java.lang.*;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+        A obj= new B();
+        System.out.println(obj.meth());
+
+    }
+
 }
